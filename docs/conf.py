36890b3184ab74b24e82abe0981c1d7a94b360a1
Pradyun Gedam <pradyunsg@gmail.com>
2020-07-14 18:27:47 +0530

    Borrow the mkdocs-material logo for now

--- docs/conf.py
+++ docs/conf.py
@@ -49,3 +49,6 @@ html_theme = "materialism"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["static"]
+
+html_title = "Materialism for Sphinx"
+html_logo = "static/logo.svg"
